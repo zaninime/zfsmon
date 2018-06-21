0505aa4a7f3c78d62b9bd0b19f086ecb4a2caf03
Francesco Zanini <francesco@zanini.me>
2018-06-21 12:10:04 +0200

    Fix import path

--- cmd/zfs_exporter/main.go
+++ cmd/zfs_exporter/main.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"zanini.me/zfsmon/clif"
+	"github.com/zaninime/zfsmon/clif"
 )
 
 var (
